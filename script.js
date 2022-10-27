2965fb5b92a003dbabe4894b228f75495658d84e
rsankutty <r.sankutty@gmail.com>
2022-10-27 00:27:06 -0300

    HTML e CSS prontos

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
- 
+let name = prompt("Seu lindo nome");
+console.log(name);
+
+function enterEvent(event) {
+  if (event.keyCode == 13) {
+    clearInput();
+ }
+}
+
+function clearInput(){
+  var getValue= document.getElementById("message");
+    if (getValue.value !="") {
+        getValue.value = "";
+    }
+}
+
+function sendClick(){
+  clearInput();
+}
